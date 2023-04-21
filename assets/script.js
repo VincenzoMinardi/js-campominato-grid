1350ae5ba2b70fc738823fcacc591de1acea2861
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-21 13:59:42 +0200

    add: event-listener cell

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,20 @@
 
 const eleBtn = document.querySelector('#btn-play')
 const eleGrid = document.querySelector('.grid')
-const eleCell = document.querySelector('.cell')
+
 
 // Generiamo la griglia desiderata
 Grid =(100)
+
+// applichiamo gli event listener alla griglia che contiene le celle
+
+eleCell = document.querySelector('.cell')
+for (let i = 0; i < eleCell.length; i++){
+    cell = eleCell.length[i]
+    cell.addEventListener('click',
+		function colorCell() {
+			console.log(this);
+			this.classList.toggle('clicked');
+		}
+	);
+}
