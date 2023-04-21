2210c2682aa1166775a2ed5131c532ffb05ff9a0
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-21 13:53:01 +0200

    add: ele-grid and ele-cell

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 // Selezionare il bottone dentro l'HTML
 
 const eleBtn = document.querySelector('#btn-play')
+const eleGrid = document.querySelector('.grid')
+const eleCell = document.querySelector('.cell')
+
+// Generiamo la griglia desiderata
+Grid =(100)
