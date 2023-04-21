7a4c3d97c1e8ed82ccb6341bdee3456a226addc7
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-21 15:25:48 +0200

    add: grid script

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,47 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
 // Selezionare il bottone dentro l'HTML
 
-const eleBtn = document.querySelector('#btn-play')
-const eleGrid = document.querySelector('.grid')
+const eleBtn = document.querySelector('#btn-play');
+console.log(eleBtn);
+const eleGrid = document.querySelector('.grid');
+console.log(eleGrid);
 
 
 // Generiamo la griglia desiderata
-Grid =(100)
+ createGrid (100, eleGrid)
 
 // applichiamo gli event listener alla griglia che contiene le celle
 
-eleCell = document.querySelector('.cell')
-for (let i = 0; i < eleCell.length; i++){
-    cell = eleCell.length[i]
-    cell.addEventListener('click',
+const listCells = document.querySelectorAll('.cell');
+for (let i = 0; i < listCells.length; i++) {
+	const cell = listCells[i];
+	cell.addEventListener('click',
 		function colorCell() {
 			console.log(this);
 			this.classList.toggle('clicked');
 		}
 	);
 }
+
+
+
+// FUNCTION DEFINITIONS
+
+function createGrid(numberCell, eleContainer){
+    for (let i= 0; i < numberCell; i++){
+        eleContainer.innerHTML += '<div class="cell"></div>';
+    }
+    
+}
+
+function numberCell (number){
+    for (let i = 1; i >= 100; i++){
+        cell.innerHTML += `<div class ="cell">${number}</div>`
+    }
+}
+
+
